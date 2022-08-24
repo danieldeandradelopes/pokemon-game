322a507a29efe5c5bf168efaad96c996baa154a4
Daniel <danieldeandradelopes@gmail.com>
2022-08-24 15:58:25 -0300

    add moves to pokemons

--- js/script.js
+++ js/script.js
@@ -24,23 +24,53 @@ function getTopPosition() {
   return parseInt(ash.style.top.split('px')[0]) || 2;
 }
 
-function verifyLookPokemon() {
+let findCharmander = false;
+let findPikaxu = false;
+let findZubat = false;
+
+function verifyLookPokemon(to) {
+
+  const pokemonRightPosition = to === 'ArrowLeft' ? `${getRightPosition() - 64}px` : `${getRightPosition() + 64}px`;
+
+  if (findCharmander) {
+    const newTopPosition = to === 'ArrowUp' ? `${getTopPosition() + 8}px` : `${getTopPosition() - 8}px`;
+
+    charmander.style.right = pokemonRightPosition;
+    charmander.style.top = newTopPosition;
+  }
+
+  if (findPikaxu) {
+    const newTopPosition = to === 'ArrowUp' ? `${getTopPosition() + 36}px` : `${getTopPosition() - 36}px`;
+
+    pikaxu.style.right = pokemonRightPosition;
+    pikaxu.style.top = newTopPosition;
+  }
+
+  if (findZubat) {
+    const newTopPosition = to === 'ArrowUp' ? `${getTopPosition() + 72}px` : `${getTopPosition() - 72}px`;
+
+    zubat.style.right = pokemonRightPosition;
+    zubat.style.top = newTopPosition;
+  }
 
   if ((getTopPosition() >= 2 && getTopPosition() <= 98)
     && (getRightPosition() >= 130 && getRightPosition() <= 216)) {
     charmander.style.display = 'block';
+    findCharmander = true;
     return;
   }
 
   if ((getTopPosition() >= 474 && getTopPosition() <= 594)
     && (getRightPosition() <= 138 && getRightPosition() >= 42)) {
     zubat.style.display = 'block';
+    findZubat = true;
     return;
   }
 
   if ((getTopPosition() >= 266 && getTopPosition() <= 394)
     && (getRightPosition() >= 546 && getRightPosition() <= 650)) {
     pikaxu.style.display = 'block';
+    findPikaxu = true;
     return;
   }
 
@@ -77,5 +107,5 @@ body.addEventListener('keydown', (event) => {
       break;
   }
 
-  verifyLookPokemon();
+  verifyLookPokemon(event.code);
 });
